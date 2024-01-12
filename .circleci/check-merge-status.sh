3bbdf30ffa5b09bc3820f9090d1fc21e3f63d752
Stayko Chalakov <stayko.chalakov@gocity.com>
2024-01-12 18:06:11 +0000

    ci: update

--- .circleci/check-merge-status.sh
+++ .circleci/check-merge-status.sh
@@ -15,11 +15,6 @@ echo "mergeStateStatus: $merge_state_status"
 
 if [ "$mergeable" != "MERGEABLE" ] || [ "$merge_state_status" != "BLOCKED" ]; then
   echo "PR has conflicts or is out of date with the base branch! Aborting job."
-  if [ "$merge_state_status" = "BEHIND" ]; then
-    echo "Rebasing the PR branch with the base branch..."
-    git rebase main
-    git push -f
-  fi
   # exit and prevent any later steps from running
   exit 1
 fi
